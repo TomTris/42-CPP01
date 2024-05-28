2a53c84f86f8d39f558f058ec4d2582171d0c352
TomTris <quoctrido.jb@gmail.com>
2024-05-28 05:16:34 +0200

    ex01

---
+++ ex01/Zombie.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qdo <qdo@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/28 04:10:37 by qdo               #+#    #+#             */
+/*   Updated: 2024/05/28 05:15:52 by qdo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+std::string Zombie::getName(void) {return name;}
+Zombie::Zombie() {}
+Zombie::~Zombie()
+{
+	std::cout << name << ": die" << std::endl;	
+}
+
+void	Zombie::setName(std::string nameOutside)
+{
+	name = nameOutside;
+	std::cout << name << ": is born" << std::endl;
+}
