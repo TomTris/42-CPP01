57b498835bbdee25fa6706423d24ed4ad84381eb
TomTris <quoctrido.jb@gmail.com>
2024-05-29 04:51:58 +0200

    msg with newline

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -6,15 +6,15 @@
 /*   By: qdo <qdo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 04:11:41 by qdo               #+#    #+#             */
-/*   Updated: 2024/05/29 04:37:38 by qdo              ###   ########.fr       */
+/*   Updated: 2024/05/29 04:49:19 by qdo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
 void Harl::debug(void) { std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl; }
-void Harl::info(void) { std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl; }
-void Harl::warning(void) { std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl; }
+void Harl::info(void) { std::cout << "I cannot believe adding extra bacon costs more money.\nYou didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl; }
+void Harl::warning(void) { std::cout << "I think I deserve to have some extra bacon for free.\nI’ve been coming for years whereas you started working here since last month." << std::endl; }
 void Harl::error(void) { std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl; }
 
 void	Harl::complain( std::string level )
