9bee55c5ba540e1f34f3031c04397580b0e1b40a
TomTris <quoctrido.jb@gmail.com>
2024-05-29 04:36:13 +0200

    ex05

---
+++ ex05/Harl.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qdo <qdo@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/29 02:45:04 by qdo               #+#    #+#             */
+/*   Updated: 2024/05/29 04:28:58 by qdo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <string>
+# include <iostream>
+#include <iostream>
+#include <functional>
+
+class Harl
+{
+
+public:
+	typedef void (Harl::*MemberFunction) (void);
+	void	complain( std::string level );
+
+private:
+	void debug(void);
+	void info(void);
+	void warning(void);
+	void error(void);
+};
+
+
+#endif
